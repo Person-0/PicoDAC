a32b9758aa24475bab554032dbe6b5ec45b5a9ca
Person-0 <79051792+Person-0@users.noreply.github.com>
2026-08-29 12:22:45 +0530

    neopixel is onboard

--- firmware/src/pindefs.h
+++ firmware/src/pindefs.h
@@ -1,17 +1,18 @@
 // dac pins
-#define PIN_DAC_MCLK        0
-#define PIN_DAC_SCLK1       1
-#define PIN_DAC_LCRK1       2
-#define PIN_DAC_SDIN1       3
-#define PIN_DAC_RESET       4
-#define PIN_DAC_SDA         6
-#define PIN_DAC_SCL         7
+#define PIN_DAC_MCLK                0
+#define PIN_DAC_SCLK1               1
+#define PIN_DAC_LCRK1               2
+#define PIN_DAC_SDIN1               3
+#define PIN_DAC_RESET               4
+#define PIN_DAC_SDA                 6
+#define PIN_DAC_SCL                 7
 
 // level shifter output enable
-#define PIN_MCU_LS_OE       5
+#define PIN_MCU_LS_OE               5
 
 // led pins
-#define PIN_NEOPIXEL        22
-#define PIN_NEOPIXEL_PWR    23
-#define PIN_ONBOARD_LED     25
-#define PIN_LED_CONTROL     26
+#define PIN_ONBOARD_NEOPIXEL        22
+#define PIN_ONBOARD_NEOPIXEL_PWR    23
+#define PIN_ONBOARD_LED             25
+
+#define PIN_LED_CONTROL             26
